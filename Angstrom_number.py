13b0d589c9f77d6748cda3802d2b9318c00a9ca2
MaanasLimkar35 <45557115+MaanasLimkar35@users.noreply.github.com>
2018-12-20 08:12:27 +0530

    Add files via upload

---
+++ Angstrom_number.py
@@ -0,0 +1,15 @@
+#Name: Maanas Limkar
+#Gr no:11810058
+#roll no:35
+#Division:M
+def armstrong(n):                                #function to define armstrong number
+    sum=0
+    t=n
+    while t>0:
+        d=t%10                                   #Separating out the number into indiviual digits
+        sum=t**3                                 #cube of the digits is found out
+        t=t//10
+    return sum
+n=int(input("Enter a number"))                   #user input number is taken    
+y=armstrong(n)                                   #function is called        
+print(y)              
